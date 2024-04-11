cc1e89ee59247877d33f24e968aa24740544aba0
Gabriela Pereda <gabrielapereda@Gabrielas-MBP.wired.duke.local>
2024-04-11 14:31:01 -0400

    dijkstras v0.1

---
+++ algs/Djikstra.py
@@ -0,0 +1,9 @@
+from algs.SuperTraverser import SuperTraverser
+
+class Djikstra(SuperTraverser):
+    def __init__(self, graph):
+        super().__init__(graph)
+
+    def traverse(self, start_node_name, end_node_name):
+
+
